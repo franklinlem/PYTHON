e8a4f37127c842263799e2541b377f535696c664
franklinlem <franklinlem@yahoo.com>
2023-08-30 01:32:26 -0300

    aulas de python

--- Aulas Guanabara/aula003.py
+++ Aulas Guanabara/aula003.py
@@ -3,3 +3,5 @@
 frase = "Curso em Video Python"
 print(frase[9:14])
 print(frase[0:20:2])
+nome = str(input("Qual é o seu nome completo?")).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
---
+++ Aulas Guanabara/aula004.py
@@ -0,0 +1,8 @@
+nome=str(input("Qual o seu nome?"))
+print(nome)
+idade=int(input("Qual a sua idade?"))
+print(idade)
+if idade >= 50:
+    print("Você está velho! kkkk")
+else:
+    print("Ainda tem chão pela frente!!!")
