f43c26a10978628d572d77f1f987725c281cf229
franklinlem <franklinlem@yahoo.com>
2024-11-03 09:48:13 -0300

    estudando python

--- livroPythonDjango/erro.py
+++ livroPythonDjango/erro.py
@@ -1,12 +1,7 @@
-#O famoso "Hello, World!"
-print("Hello, World!")
-
-#calculos simples
-
 altura = 1.8
 largura = 5.0
 comprimento = 6.0
-area_base = largura * comprimento
+area_base = largura * comprimento #potencia em vez de multiplicação não informa erro
 volume = altura * area_base
 
 print("Área da base:", area_base)
