4547e8ea2df7a7627e71c915ffe0ee18982cf9b6
franklinlem <franklinlem@yahoo.com>
2024-11-30 13:02:07 -0300

    estudando django

--- appweb/aplicacaoweb01/views.py
+++ appweb/aplicacaoweb01/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -16,6 +19,24 @@ def topics(request):
 def topic(request, topic_id):
     """Assunto e suas entradas"""
     topic = Topic.objects.get(id = topic_id)
-    entries = topic.entry_set.order_by('-date_added')
+    entries = topic.entry_set.order_by('-date_added') # type: ignore
     context = {'topic': topic, 'entries': entries}
     return render(request, 'aplicacaoweb01/topic.html', context)
+
+def new_topic(request):
+    """Adicionar novo tópico"""
+    if request.method != 'POST':
+        #Criar form em branco se nada for enviado
+        form = TopicForm()
+    else:
+        #Criar form com dados enviados pelo usuário
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            #Salvar o novo tópico
+            form.save()
+            return topics(request)
+            #return HttpResponseRedirect('/topics/')
+            #return HttpResponseRedirect(reverse('topics'))
+        
+    context = {'form':form}        
+    return render(request, 'aplicacaoweb01/new_topic.html', context)
