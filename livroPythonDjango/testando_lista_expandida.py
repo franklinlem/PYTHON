dddc4b285d62ec08a3642542baf8f6500643cdc6
Franklin Martins <franklinlem@yahoo.com>
2025-06-04 12:00:46 -0300

    estudando python

--- livroPythonDjango/testando_lista_expandida.py
+++ livroPythonDjango/testando_lista_expandida.py
@@ -9,3 +9,22 @@ def sao_impares(*lista):
     return resultado
 
 print(sao_impares(1,3,5,7,9))
+
+"""Convertendo parametros em um dicionario
+
+def altera_registro(**kwargs):
+    nome = kwargs['nome']
+    codigo = kwargs['codigo']
+
+altera_registro(
+    codigo = 1,
+    nome = 'Francisco'
+)
+
+registro = {
+    'codigo':1,
+    'nome':'Francisco'
+}
+
+altera_registro(**registro)
+"""
